72428a75ecd42c66377d25d130a58b19eb917efa
fredricklou523 <fredrick.lou@gmail.com>
2018-11-07 18:52:11 -0800

    base file

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = ({ mode } = { mode: 'production' }) => {
+	// console.log(mode);
+	return {
+		mode,
+		output: {
+			filename: 'bundle.js',
+			hotUpdateChunkFilename: 'hot/hot-update.js',
+			hotUpdateMainFilename: 'hot/hot-update.json'
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				title: 'Pomocode',
+				template: './src/index.html'
+			}),
+			new webpack.ProgressPlugin()
+		],
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-react', '@babel/preset-env']
+						}
+					}
+				},
+				{
+					test: /\.(jpg|png|svg)$/,
+					loader: 'file-loader',
+					options: {
+						name: '[path][name].[ext]'
+					}
+				}
+			]
+		},
+		resolve: {
+			extensions: ['.js', '.jsx']
+		}
+	};
+};
