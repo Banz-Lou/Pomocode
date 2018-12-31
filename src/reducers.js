9031e71692b5afc6ba19b56db3c94b780ef9ff53
Michael Banzon <mrnbanzon@gmail.com>
2018-12-31 09:11:22 -0800

    Setup for Redux reducers commented.

--- src/reducers.js
+++ src/reducers.js
@@ -1,19 +1,39 @@
 import { SET_SEARCH_TERM } from './actions';
 
 const DEFAULT_STATE = {
-	searchTerm: ''
+  searchTerm: ''
 };
 
 const setSearchTerm = (state, action) =>
-	Object.assign({}, state, { searchTerm: action.payload });
+  Object.assign({}, state, { searchTerm: action.payload });
 
 const rootReducer = (state = DEFAULT_STATE, action) => {
-	switch (action.type) {
-		case SET_SEARCH_TERM:
-			return setSearchTerm(state, action);
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case SET_SEARCH_TERM:
+      return setSearchTerm(state, action);
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
+
+/*
+state: 
+	intervals: [{}, {}, ...]  //top 5
+	plans: {
+		repos: [{}, {}, ...] //load ALL repos
+		issues: [{}, {}, ...] //pre-load ALL issues
+		selectedRepo: {} OR repo.id
+		selectedIssue: {} OR issue.id
+		seconds: integer
+		dates: date range
+	}
+	activeIssues: [{}, {}, ...] //list of active issues; NOTE: COMPLETED button?
+	historical: [{}, {}, ...]
+	details: {
+		stuffMcTings: // which one to show?
+		filesIntervals: [{}, {}, ...]
+		issuesIntervals: [{}, {}, ...]
+	}
+*/
