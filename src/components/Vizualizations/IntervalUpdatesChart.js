b0a24a7081f3928aa697b974911ba02f9b83b27a
Fredrick <fredrick@Fredricks-iMac.local>
2019-04-18 18:02:59 -0700

    BASIC INTERVAL UPDATES VIZ COMPLETE

--- src/components/Vizualizations/IntervalUpdatesChart.js
+++ src/components/Vizualizations/IntervalUpdatesChart.js
@@ -1,140 +1,109 @@
-import React, { Component } from 'react';
-import * as d3 from 'd3';
-import { HistDetDimensions } from './dimensions.js';
+import React, { Component } from "react";
+import * as d3 from "d3";
+import { IntUpdDimensions } from "./dimensions.js";
 
-const { width, height, margin, colors } = HistDetDimensions;
-
-//What we want before .map
-
-// var data = [ {int:1, issue:1 },{int:1, issue:2}, {int:2, issue:1},{int:3, issue:1}];
-
-// function testTransform(data){
-// var max = 3;
-
-// var obj = {};
-// obj[max] = [];
-// obj[max-1] = [];
-// obj[max-2]= [];
-
-// console.log(obj);
-// data.forEach(issue => {
-//     obj[issue.int].push(issue);
-// })
-
-// return Object.values(obj);
-
-// }
-
-// testTransform(data)
-
-// var issueData =  {int:1, issue_id:1, issue: {plan_seconds: 100, issue_name: 'TEST'}, prior_active: 20, total_active: 30  }
-
-// let array = [];
-// let objPlan = {category: 'plan', plan: issueData.issue.plan_seconds};
-// let objActive = {category: 'active', prior_active: issueData.prior_active, active: issueData.total_active};
+const { width, height, margin, colors } = IntUpdDimensions;
 
 let data = [
-	{
-		id: 1,
-		type: 'Plan',
-		plan_seconds: 3840
-	},
-	{
-		id: 1,
-		type: 'Active',
-		prior_active: 3000,
-		active: 200
-	}
+  {
+    id: 1,
+    type: "Plan",
+    plan_seconds: 3840
+  },
+  {
+    id: 1,
+    type: "Active",
+    prior_active: 3000,
+    active: 600
+  }
 ];
 
 class IntervalUpdatesChart extends Component {
-	state = {
-		bars: []
-	};
+  state = {
+    bars: []
+  };
 
-	static getDerivedStateFromProps(nextProps, prevState) {
-		//Create stack data
-		let stack = d3
-			.stack()
-			.keys(['prior_active', 'active'])
-			.order(d3.stackOrderNone)
-			.offset(d3.stackOffsetNone);
+  static getDerivedStateFromProps(nextProps, prevState) {
+    //Create stack data
+    let stack = d3.stack().keys(["prior_active", "active"]);
 
-		let stackData = stack([data[1]]);
-		let activeBars = [];
+    let stackData = stack([data[1]]);
+    let activeBars = [];
 
-		// obtain y max
-		let yMax = data.reduce((max, obj) => {
-			let totalActive = obj.prior_active + obj.active || 0;
-			let planSeconds = obj.plan_seconds || 0;
-			return Math.max(max, totalActive, planSeconds);
-		}, 0);
+    // obtain y max
+    let xMax = data.reduce((max, obj) => {
+      let totalActive = obj.prior_active + obj.active || 0;
+      let planSeconds = obj.plan_seconds || 0;
+      return Math.max(max, totalActive, planSeconds);
+    }, 0);
 
-		// create y scale
-		const yScale = d3
-			.scaleLinear()
-			.domain([0, yMax])
-			.range([height - margin.bottom, margin.top]);
+    // create x scale
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, xMax])
+      .range([0, width - margin.right]);
 
-		// create x scale
-		const xExtent = d3.extent(data, d => d.id);
-		const xScale = d3
-			.scaleLinear()
-			.domain(xExtent)
-			.range([margin.left, width - margin.right]);
+    // create y scale
+    const yExtent = d3.extent(data, d => d.id);
+    const yScale = d3
+      .scaleLinear()
+      .domain(yExtent)
+      .range([0, height]);
 
-		const colorScale = d3
-			.scaleOrdinal()
-			.domain(stackData.keys())
-			.range(['orange', 'green']);
+    // STACKED CHART color scale
+    const colorScale = d3
+      .scaleOrdinal()
+      .domain(stackData.keys())
+      .range([colors.active, colors.prior_active]);
 
-		stackData.forEach(index => {
-			let result = index.map(d => {
-				return {
-					x: xScale(d.id),
-					y: yScale(d[1]),
-					height: yScale(d[0]) - yScale(d[1]),
-					fill: colorScale(index.key)
-				};
-			});
-			activeBars = activeBars.concat(result);
-		});
-		//console.log(activeBars);
+    //ACTIVE -> STACKED CHART
+    stackData.forEach((bar, i) => {
+      console.log(bar);
+      let xpos = i > 0 ? bar[0][0] : 0;
+      let result = {
+        y: yScale(bar[0].data.id),
+        x: xScale(xpos),
+        width: xScale(bar[0][1]) - xScale(bar[0][0]),
+        fill: colorScale(bar.key)
+      };
+      activeBars = activeBars.concat(result);
+    });
 
-		const planBars = [data[0]].map(d => {
-			return {
-				x: xScale(d.id),
-				y: yScale(d.plan_seconds),
-				height: yScale(0) - yScale(d.plan_seconds),
-				fill: 'blue'
-			};
-		});
+    // PLANNED BAR
+    const planBars = [data[0]].map(d => {
+      return {
+        y: yScale(d.id),
+        x: xScale(0),
+        width: xScale(d.plan_seconds) - xScale(0),
+        fill: colors.planned
+      };
+    });
 
-		return { activeBars, planBars };
-	}
+    return { activeBars, planBars };
+  }
 
-	render() {
-		return (
-			<svg width={width} height={height}>
-				<g transform="rotate(90 200 200)">
-					{this.state.planBars.map(d => (
-						<rect x={d.x} y={d.y} width={10} height={d.height} fill={d.fill} />
-					))}
-				</g>
-				<g transform="rotate(90 200 200)">
-					{this.state.activeBars.map(d => (
-						<rect
-							x={d.x + 10}
-							y={d.y}
-							width={10}
-							height={d.height}
-							fill={d.fill}
-						/>
-					))}
-				</g>
-			</svg>
-		);
-	}
+  render() {
+    return (
+      <svg width={width} height={height} style={{ border: "solid 5px" }}>
+        <g>
+          {this.state.planBars.map(d => (
+            <rect x={d.x} y={d.y} width={d.width} height={10} fill={d.fill} />
+          ))}
+        </g>
+        <g>
+          {this.state.activeBars.map(d => (
+            <rect
+              x={d.x}
+              y={d.y + 10}
+              width={d.width}
+              height={10}
+              fill={d.fill}
+            />
+          ))}
+        </g>
+      </svg>
+    );
+  }
 }
 
 export default IntervalUpdatesChart;
--- src/components/Vizualizations/dimensions.js
+++ src/components/Vizualizations/dimensions.js
@@ -1,22 +1,36 @@
 const HistDetDimensions = {
-	width: 650,
-	height: 400,
-	margin: { top: 20, right: 40, bottom: 20, left: 35 },
-	colors: {
-		planned: '#98abc5',
-		actual: '#ff8c00',
-		difference: 'maroon'
-	},
-	legend: {
-		rect: 2,
-		spacing: 2,
-		height: 2
-	}
+  width: 650,
+  height: 400,
+  margin: { top: 20, right: 40, bottom: 20, left: 35 },
+  colors: {
+    planned: "#98abc5",
+    actual: "#ff8c00",
+    difference: "maroon"
+  },
+  legend: {
+    rect: 2,
+    spacing: 2,
+    height: 2
+  }
 };
 
-const IntUpdDimensions = {};
+const IntUpdDimensions = {
+  width: 650,
+  height: 100,
+  margin: { top: 20, right: 40, bottom: 20, left: 35 },
+  colors: {
+    planned: "#98abc5",
+    active: "#ff8c00",
+    prior_active: "maroon"
+  },
+  legend: {
+    rect: 2,
+    spacing: 2,
+    height: 2
+  }
+};
 
 module.exports = {
-	HistDetDimensions,
-	IntUpdDimensions
+  HistDetDimensions,
+  IntUpdDimensions
 };
